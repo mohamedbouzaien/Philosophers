d6009740b3e80efa2f1b9491e1c349049340697d
Mohamed Bouzaien <mbouzaie@student.42.fr>
2021-12-21 17:19:13 +0100

    sleep and think

--- incs/philosophers.h
+++ incs/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mbouzaie <mbouzaie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 16:32:02 by mbouzaie          #+#    #+#             */
-/*   Updated: 2021/12/17 20:11:14 by mbouzaie         ###   ########.fr       */
+/*   Updated: 2021/12/21 13:41:22 by mbouzaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,32 @@
 # define PHILOSOPHERS_H
 
 # include <unistd.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
 
-typedef struct s_rules
+struct	s_args;
+
+typedef struct s_philo
 {
-	int		number;
-	int		die_t;
-	int		eat_t;
-	int		sleep_t;
-	int		eat_n;
-} t_rules;
+	int				id;
+	pthread_t		thread;
+	pthread_mutex_t	fork;
+	struct s_args	*args;
+
+}	t_philo;
+
+typedef struct s_args
+{
+	int				number;
+	int				die_t;
+	int				eat_t;
+	int				sleep_t;
+	int				eat_n;
+	uint64_t		start_t;
+	pthread_mutex_t	write_m;
+	t_philo			**philos;
+} t_args;
 
 int	ft_atoi(const char *str);
 void	print_action(int timestamp, int philo, char *action);
--- srcs/main.c
+++ srcs/main.c
@@ -6,25 +6,84 @@
 /*   By: mbouzaie <mbouzaie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 17:49:55 by mbouzaie          #+#    #+#             */
-/*   Updated: 2021/12/17 20:14:38 by mbouzaie         ###   ########.fr       */
+/*   Updated: 2021/12/21 13:57:47 by mbouzaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/philosophers.h"
 #include <stdio.h>
 
+uint64_t	get_time(void)
+{
+	static struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * (uint64_t)1000) + (time.tv_usec / 1000));
+}
+
+void	do_actions(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->args->write_m));
+	print_action(get_time() - philo->args->start_t, philo->id, " is sleeping\n");
+	pthread_mutex_unlock(&(philo->args->write_m));
+	usleep(philo->args->sleep_t * 1000);
+	pthread_mutex_lock(&(philo->args->write_m));
+	print_action(get_time() - philo->args->start_t, philo->id, " is thinking\n");
+	pthread_mutex_unlock(&(philo->args->write_m));
+}
+
+void	*life_cycle(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	while (1)
+	{
+		do_actions(philo);
+	}
+	return (NULL);
+}
+
+int	init(t_args *args)
+{
+	int	i;
+
+	pthread_mutex_init(&(args->write_m), NULL);
+	args->philos = (t_philo **)malloc(sizeof(t_philo *) * (args->number + 1));
+	args->start_t = get_time();
+	i = 0;
+	while (i < args->number)
+	{
+		args->philos[i] = (t_philo *)malloc(sizeof(t_philo) + 1);
+		args->philos[i]->id = i + 1;
+		args->philos[i]->args = args;
+		i++;
+	}
+	i = 0;
+	while (i < args->number)
+	{
+		if (pthread_create(&(args->philos[i]->thread), NULL, life_cycle, args->philos[i]) != 0)
+			return (1);
+		usleep(100);
+		i++;
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
-	t_rules	rules;
+	t_args	*args;
 
+	args = (t_args *)malloc(sizeof(t_args) + 1);
 	if (ac > 4)
 	{
-		rules.number = ft_atoi(av[1]);
-		rules.die_t = ft_atoi(av[2]);
-		rules.eat_t = ft_atoi(av[3]);
-		rules.sleep_t = ft_atoi(av[4]);
+		args->number = ft_atoi(av[1]);
+		args->die_t = ft_atoi(av[2]);
+		args->eat_t = ft_atoi(av[3]);
+		args->sleep_t = ft_atoi(av[4]);
 		if (ac == 6)
-			rules.eat_n = ft_atoi(av[5]);
+			args->eat_n = ft_atoi(av[5]);
+		return (init(args));
 	}
-	print_action(300, 1, " has taken a fork\n");
+	return (0);
 }
